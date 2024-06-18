8f4a84f596e3818360f02ee19236454291ac6177
Abhishek <168292406+AbhiGitLab@users.noreply.github.com>
2024-06-19 01:10:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@
        this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
      }
       /*===== Nav Toggler =====*/
-    //   const navMenu = document.querySelector(".menu");
-    //   navToggle = document.querySelector(".menu-btn");
-    //   if(navToggle)
-    //   {
-    //       navToggle.addEventListener("click", () =>
-    //       {
-    //           navMenu.classList.toggle("active");
-    //       })
-    //   }
+       const navMenu = document.querySelector(".menu");
+        navToggle = document.querySelector(".menu-btn");
+       if(navToggle)
+       {
+           navToggle.addEventListener("click", () =>
+           {
+               navMenu.classList.toggle("active");
+           })
+      }
       // closing menu when link is clicked
       const navLink = document.querySelectorAll(".nav-link");
       function linkAction()
